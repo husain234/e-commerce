7ed21702e0793b43126d8e484504e53e65157e4d
husain234 <48036941+husain234@users.noreply.github.com>
2019-11-15 13:08:53 +0700

    admin Pangan

---
+++ application/views/pangan/v_komuditi.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+<?php $this->load->view('pasar/head') ?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+<?php $this->load->view('pasar/header') ?>
+  <!-- Left side column. contains the logo and sidebar -->
+  
+<?php $this->load->view('pasar/leftbar') ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <small>Version 2.0</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <!-- Info boxes -->
+      
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+<?php $this->load->view('pasar/footer') ?>
+</body>
+</html>
