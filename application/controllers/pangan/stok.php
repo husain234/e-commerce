a9599576165bca0fa716a2ea03a6b0319946c531
husain234 <48036941+husain234@users.noreply.github.com>
2019-11-22 22:07:41 +0700

    pengurangan

--- application/controllers/pangan/stok.php
+++ application/controllers/pangan/stok.php
@@ -11,7 +11,7 @@ class stok extends CI_Controller
 		if (empty($user)) {
 			redirect('../login/logout');
 		} else {
-			if ($user == 'pangan') {
+			if ($user == 'Admin Ketahanan Pangan') {
 				$this->load->view('pangan/v_stok');
 			} else {
 				redirect('../login/logout');
